5ea0829ca12c9b79c5f33411078cf8e5fc267ecc
Chloe Horgan <super.chloe@gmail.com>
2016-06-20 20:12:08 -0400

    Initial Commit

---
+++ SwiftReadability/SwiftReadability.h
@@ -0,0 +1,19 @@
+//
+//  SwiftReadability.h
+//  SwiftReadability
+//
+//  Created by Chloe on 2016-06-20.
+//  Copyright © 2016 Chloe Horgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftReadability.
+FOUNDATION_EXPORT double SwiftReadabilityVersionNumber;
+
+//! Project version string for SwiftReadability.
+FOUNDATION_EXPORT const unsigned char SwiftReadabilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftReadability/PublicHeader.h>
+
+
